70db6c10672a50f12813f04042b3e4d40e527b90
Marta Odrzywolek <odrzywolek.m@gmail.com>
2023-06-02 20:02:37 +0200

    initial commit

---
+++ Strawberry/StrawberryApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrawberryApp.swift
+//  Strawberry
+//
+//  Created by Majka on 02/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct StrawberryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
